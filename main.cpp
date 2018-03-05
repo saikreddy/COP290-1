a8c8c5f9f315868a19e187f80a8714ac338b6119
Brajmohan Meena <33186704+brajmeena@users.noreply.github.com>
2018-03-05 19:17:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <cmath>
+#include <vector>
+#include <list>
 
 using namespace  std;
 
-class point{
-	int x_cor;
-	int y_cor;
-	int z_cor;
+class Points{
+	float x_cor;
+	float y_cor;
+	float z_cor;
 
-	void edge (point ){
+	std::list<Points> Neigh;
+	/**
+	 * gives the points
+	 *
+	 */
+
+	void edge (points){
 
 	}
 };	
 
-void front_view(points){
-}
 
-void side_view(points){
-}
-
-
-class twoD_projection{
+class makethreeD{
 	top_view;
 	front_view;
 	side_view;
 
-	int create3D()
+	int create3D( top_view, front_view, side_view){
+		return 0;
+	}
 
+
+}
+class otho_projection{
+	
 
 }
 
