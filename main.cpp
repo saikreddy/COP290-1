396ac9c15ff1d91ce7447168099ffde1ed6ea4ee
Brajmohan Meena <33186704+brajmeena@users.noreply.github.com>
2018-03-05 13:50:41 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cmath>
+
+using namespace  std;
+
+class point{
+	int x_cor;
+	int y_cor;
+	int z_cor;
+
+	void edge (point ){
+
+	}
+};	
+
+void front_view(points){
+}
+
+void side_view(points){
+}
+
+
+class twoD_projection{
+	top_view;
+	front_view;
+	side_view;
+
+	int create3D()
+
+
+}
+
+int main (void) {
+	return 0;
+}
