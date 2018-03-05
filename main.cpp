2d29464b13baa46890f6606983ab3df05ebd3c1a
Brajmohan Meena <33186704+brajmeena@users.noreply.github.com>
2018-03-05 20:41:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,36 +5,52 @@
 
 using namespace  std;
 
-class Points{
+class Vertex{
 	float x_cor;
 	float y_cor;
-	float z_cor;
-
-	std::list<Points> Neigh;
-	/**
-	 * gives the points
-	 *
-	 */
-
-	void edge (points){
-
-	}
+	float z_cor;	
 };	
 
+class Edge{
+	Vertex p1, p2;
+}
 
 class makethreeD{
 	top_view;
 	front_view;
 	side_view;
+public :
 
-	int create3D( top_view, front_view, side_view){
-		return 0;
+		int create3D( top_view, front_view, side_view){
+			return 0;
 	}
 
 
 }
-class otho_projection{
-	
+class twoDto3D
+{
+public :
+		
+		/**
+		 creating the front view from an image
+		 */
+		void createFrontView(std::list<Points> plane){
+
+		}
+		
+		/**
+		 creating the side view from an image
+		 */		
+		void createSideView(std::list<Points> plane){
+
+		}
+
+		/**
+		 creating the top view from an image
+		 */
+		void createTopView(std::list<Points> plane ){
+
+		}
 
 }
 
