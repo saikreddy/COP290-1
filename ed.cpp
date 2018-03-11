58968ced4df135bc07829c2ed4c7f10071806000
Brajmohan Meena <33186704+brajmeena@users.noreply.github.com>
2018-03-12 00:05:39 +0530

    Add files via upload
    new file using vectors

---
+++ ed.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class vertex{
+	float x;
+	float y;
+	float z;
+
+	std::vector<vertex> v;
+
+};
+
+class projection{
+
+	std::vector<vertex> frontview;
+
+	std::vector<vertex> sideview;
+
+	std::vector<vertex> topview;
+};
+
+class twodtothreed{
+	projection front_view;
+	projection side_view;
+	projection top_view;
+
+	void crete3D(projection x, projection y, projection z){
+		
+	}
+};
+
+class threedtotwod{
+
+	std::vector<vertex> threeD_points;
+
+	void create_frontview (std::vector<vertex> x){
+		
+	}
+
+	void create_sideview (std::vector<vertex> x){
+		
+	}
+
+	void create_topview (std::vector<vertex> x){
+		
+	}
+
+};
+
+
+int main()
+{
+	return 0;
+}
